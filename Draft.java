6f075a540bacc14a1d61d002ea6e7b4313a5dec9
barolick <33667988+barolick@users.noreply.github.com>
2018-06-21 14:15:32 -0400

    Add files via upload

---
+++ Draft.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class Draft{
+  int numTeams;
+  int numPicks;
+  ArrayList<Player> undrafted;
+  ArrayList<Player> drafted;
+  League league;
+  
+  void addQB() {
+    this.undrafted.add(new QB()  
+  }
+}
