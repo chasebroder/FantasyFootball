a88019b26e1287fb2b79829801943916a50aa629
barolick <33667988+barolick@users.noreply.github.com>
2018-06-21 14:14:42 -0400

    league class

---
+++ League.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+class League{
+  double passPtsPerYd;
+  double passTD;
+  double passInt;
+  double rushPtsPerYd;
+  double rushTD;
+  double pointsPerReception;
+  double recPtsPerYD;
+  double recTD;
+  double fumble;
+  double twoPtCon;
+  double PATMade;
+  double PATMissed;
+  double FG019;
+  double FG2029;
+  double FG3039;
+  double FG4049;
+  double FG50;
+  double sack;
+  double defInt;
+  double defFumble;
+  double safety;
+  double defTD;
+  double retTD;
+  double ptAllowed0;
+  double ptAllowed16;
+  double ptAllowed713;
+  double ptAllowed1420;
+  double ptAllowed2127;
+  double ptAllowed2834;
+  double ptAllowed35;
+  
+  
+}
