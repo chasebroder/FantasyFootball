2fd4e576a3daf5115de31698cfa1921f7c774977
barolick <33667988+barolick@users.noreply.github.com>
2018-06-28 18:39:18 -0400

    updated draft class
    added sort for xValue and method for calculating need factor

--- Draft.java
+++ Draft.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 
 class Draft {
-  int numTeams;
-  int numPicks;
+  public static void Main(String[] args) {
+    
+  }
+  int pickNum;
   ArrayList<Player> undrafted;
   ArrayList<Player> drafted;
   ArrayList<QB> QBUndrafted;
@@ -12,6 +14,7 @@ class Draft {
   ArrayList<K> KUndrafted;
   ArrayList<DEF> DEFUndrafted;
   League league;
+  Team myTeam;
 
   //EFFECT: creates a new quarterback and adds it to list of undrafted players
   void addQB(String name,  int passYds, int passTDs, int rushYds, int rushTDs, int interceptions, 
@@ -55,12 +58,10 @@ class Draft {
         retTDs, ptsAllowed0, ptsAllowed16, ptsAllowed713, ptsAllowed1420, ptsAllowed2127, ptsAllowed2834,
         ptsAllowed35));
   }
-
   //Effect: Adds all the Players to a list of undrafted players
   void createUndraftedList() {
-
+    
   }
-
   //Effect: Adds all the Players to a list of their positions
   void createUndraftedPlayersList() {
     for(Player p: this.undrafted) {
@@ -132,6 +133,48 @@ class Draft {
       downHeap(leftIdx, maxIdx);
     }
   }
+  // Effect:Used when adding elements to the heap; bubbles up the element until a
+  // heap is made
+  void upHeapX(int i) {
+    int parentIdx = ((i - 1) / 2);
+    if (this.undrafted.get(i).xValue > this.undrafted.get(parentIdx).xValue) {
+      Player parent = this.undrafted.get(parentIdx);
+      Player child = this.undrafted.get(i);
+      this.undrafted.set(parentIdx, child);
+      this.undrafted.set(i, parent);
+      upHeap(parentIdx);
+    }
+  }
+  // Effect:used when sorting heap; Swaps first and last items then bubbles down
+  // until a heap is made
+  void downHeapX(int i, int maxIdx) {
+    int leftIdx = 2 * i + 1;
+    int rightIdx = 2 * i + 2;
+    int biggestIdx;
+    if (rightIdx <= maxIdx && leftIdx <= maxIdx) {
+      if (this.undrafted.get(i).xValue < this.undrafted.get(leftIdx).xValue
+          || (this.undrafted.get(i).xValue < this.undrafted.get(rightIdx).xValue)) {
+        if (this.undrafted.get(leftIdx).xValue > this.undrafted.get(rightIdx).xValue) {
+          biggestIdx = leftIdx;
+        }
+        else {
+          biggestIdx = rightIdx;
+        }
+        Player parent = this.undrafted.get(i);
+        Player child = this.undrafted.get(biggestIdx);
+        this.undrafted.set(i, child);
+        this.undrafted.set(biggestIdx, parent);
+        downHeap(biggestIdx, maxIdx);
+      }
+    }
+    if (rightIdx > maxIdx && leftIdx <= maxIdx && this.undrafted.get(i).xValue < this.undrafted.get(leftIdx).xValue) {
+      Player parent = this.undrafted.get(i);
+      Player child = this.undrafted.get(leftIdx);
+      this.undrafted.set(i, child);
+      this.undrafted.set(leftIdx, parent);
+      downHeap(leftIdx, maxIdx);
+    }
+  }
 
   // Effect:sorts the heap from lowest element value to highest
   void heapSort() {
@@ -147,13 +190,33 @@ class Draft {
       downHeap(0, end);
     }
   }
-
-  //calculates X value for every player in the list
-  //will call for each individual position
-  void calculateXVal(ArrayList<Player> players, int benchmark) {
+  //Effect:Calculates the xValue of Players
+  void calculateXValue(ArrayList<Player> players, int benchmark) {
     for(Player p: players) {
       p.xValue = p.projectedPoints - players.get(benchmark).projectedPoints;
     }
   }
-}
+  void needFactor() {
+    for(Player p: this.undrafted) {
+      if((p instanceof QB) && this.team.qbs >= 1) {
+        p.xValue = p.xValue*(1 - this.team.qbs *0.2);
+      }
+      if((p instanceof Flex) && this.team.flex == 1 && this.myTeam.rbs >= 2) {
+        p.xValue = (1 - this.team.rbs-1 *0.2);
+      }
+      if((p instanceof Flex) && this.team.flex == 1 && this.myTeam.wrs >= 2) {
+        p.xValue = (1 - this.team.wrs-1 *0.2);
+      }
+      if((p instanceof Flex) && this.team.flex == 1 && this.myTeam.tes >= 1) {
+        p.xValue = (1 - this.team.tes *0.2);
+      }
+      if((p instanceof K) && this.team.k >= 1) {
+        p.xValue = p.xValue*(1 - this.team.k *0.2);
+      }
+      if((p instanceof DEF) && this.team.def >= 1) {
+        p.xValue = p.xValue*(1 - this.team.def *0.2);
+      }
+    }
+  }
 
+}
