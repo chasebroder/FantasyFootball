e59b04c1fc2f58d7a76bb20437b6f42caf9e9902
chasebroder <33538182+chasebroder@users.noreply.github.com>
2018-06-28 18:03:15 -0400

    Update Team.java

--- Team.java
+++ Team.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
 
 class Team{
-  ArrayList<Player> myTeam;
+  ArrayList<Player> roster; //list of players on your team
+  ArrayList<Player> starters; //list of starters on your team
+  ArrayList<Player> bench; //list of players on your bench
+  int qbs; //number of quarterbacks on your team
+  int rbs; //number of running backs on your team
+  int wrs; //number of wide receivers on your team
+  int tes; //number of tight ends on your team
+  int k; //number of kickers on your team
+  int d; //number of defenses on your team
 }
