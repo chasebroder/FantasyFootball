f360564106da346174f1eff36fec679283809f47
barolick <33667988+barolick@users.noreply.github.com>
2018-07-26 19:08:47 -0400

    Update League.java

--- League.java
+++ League.java
@@ -24,13 +24,7 @@ class League {
 	double fumble;
 	double twoPtCon;
 	double PATMade;
-	double PATMissed;
-	double FG039Made;
-	double FG039Missed;
-	double FG4049Made;
-	double FG4049Missed;
-	double FG50Made;
-	double FG50Missed;
+	double FGMade;
 	double sack;
 	double defInt;
 	double defFumble;
@@ -47,8 +41,7 @@ class League {
 	League(int yourPick, int numTeams, int pickNum, int qbNum, int rbNum, int wrNum, int teNum, int flexNum,
 			int kickNum, int defNum, int benchNum, int rosterNum, double passPtsPerYd, double passTD, double passInt,
 			double rushPtsPerYd, double rushTD, double pointsPerReception, double recPtsPerYD, double recTD,
-			double fumble, double twoPtCon, double PATMade, double PATMissed, double FG039Made, double FG039Missed,
-			double FG4049Made, double FG4049Missed, double FG50Made, double FG50Missed, double sack, double defInt,
+			double fumble, double twoPtCon, double PATMade, double FGMade, double sack, double defInt,
 			double defFumble, double safety, double defTD, double retTD, double ptAllowed0, double ptAllowed713,
 			double ptAllowed1420, double ptAllowed2127, double ptAllowed2834, double ptAllowed35) {
 		this.yourPick = yourPick;
@@ -74,13 +67,7 @@ class League {
 		this.fumble = fumble;
 		this.twoPtCon = twoPtCon;
 		this.PATMade = PATMade;
-		this.PATMissed = PATMissed;
-		this.FG039Made = FG039Made;
-		this.FG039Missed = FG039Missed;
-		this.FG4049Made = FG4049Made;
-		this.FG4049Missed = FG4049Missed;
-		this.FG50Made = FG50Made;
-		this.FG50Missed = FG50Missed;
+		this.FGMade = FGMade;
 		this.sack = sack;
 		this.defInt = defInt;
 		this.defFumble = defFumble;
@@ -97,4 +84,3 @@ class League {
 	}
 
 }
-
